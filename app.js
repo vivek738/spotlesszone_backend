322c6cd181f495d59a017e0b6cd3755cbea365d0
punamoli77 <punamoli189@gmail.com>
2022-05-10 23:21:50 +0545

    connecting to the database

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const cors = require('cors')
 
 const bodyParser = require('body-parser')
 const app = express();
+//connect db
+
+dotenv.config();
 
 // to receive body data
 app.use(express.json());
@@ -20,6 +23,10 @@ app.use(bodyParser.json())
 
 // for making connection with front-end
 app.use(cors())
+
+// import databse
+
+require("./Database/db");
 
 // for making images storing directory public
 app.use(express.static(__dirname + "/Stores"));
@@ -40,7 +47,7 @@ app.use((req, res, next) => {
 
 // for server : port and host
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 const host = process.env.HOST_NAME;
 
